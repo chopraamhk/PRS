0891a0c37da87b7c16383640a274eea321755467
Mehak Chopra <m.chopra1@universityofgalway.ie>
2024-02-21 16:03:21 +0000

    Create chr_pos_to_rsid.R

---
+++ chr_pos_to_rsid.R
@@ -0,0 +1,58 @@
+Here, input is the gwas file.
+#Load packages
+```{r}
+library(BiocManager) #version 1.30.22
+library(BSgenome) #version 1.70.2
+library(SNPlocs.Hsapiens.dbSNP155.GRCh38)
+{r}
+
+#getSNPlocs()
+```{r}
+snps <- SNPlocs.Hsapiens.dbSNP155.GRCh38
+snps
+chr22_snps <- snpsBySeqname(snps, "22") #for 1 chromosome
+chr22_snps
+```
+
+#for all the chromosomes
+```{r}
+ChromosomesVec = as.character(seq(1, 22, by = 1)) 
+all_snps_info = snpsBySeqname(snps, ChromosomesVec)
+head(all_snps_info)
+all_snps_info
+all_snps_info_DF = data.frame(all_snps_info)
+```
+
+#load GWAS data 
+```{r}
+gwasData = read.delim("/path/to/gwasfile", header = TRUE)
+head(gwasData) #to see the header 
+```
+```{r}
+grGWAS_SNPs <- makeGRangesFromDataFrame(
+  gwasData, 
+  seqnames.field = 'CHR',
+  start.field = 'POS',
+  end.field = 'POS', 
+  keep.extra.columns = TRUE)
+
+head(grGWAS_SNPs)
+
+```
+#find overlaps and save the indices that are overlapped
+#qHits gives us query dataframe rows to use, which here is overlapped rows for gwasData
+#subHits gives us subject dataframe rows to use, which here is overlapped rows all_snps_info_DF
+```{r}
+qHits<- queryHits(findOverlaps(query = grGWAS_SNPs, subject = all_snps_info, type = 'within'))
+subHits <- subjectHits(findOverlaps(query = grGWAS_SNPs, subject = all_snps_info, type = 'within'))
+```
+
+#use indices to produce the original data for the overlaps
+```{r}
+overlaps <- data.frame(gwasData[qHits,], all_snps_info_DF[subHits,])
+head(overlaps)
+```
+
+```{r}
+write.csv(overlaps, "mapped_variants_gwas.csv")
+```
